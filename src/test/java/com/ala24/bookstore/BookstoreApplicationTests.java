8fb6304d5677606622a0964bf2aaaf4cbdc001c7
kookiedncream <kim76744@naver.com>
2022-02-25 21:09:28 +0900

    Initial commit

---
+++ src/test/java/com/ala24/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ala24.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
