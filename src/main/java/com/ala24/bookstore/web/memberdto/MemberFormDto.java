45b4eff96c6bdae8cd28b2c7cbb90597758a0020
kookiedncream <kim76744@naver.com>
2022-03-22 09:08:19 +0900

    test: testCase추가

--- src/main/java/com/ala24/bookstore/web/memberdto/MemberFormDto.java
+++ src/main/java/com/ala24/bookstore/web/memberdto/MemberFormDto.java
@@ -2,23 +2,23 @@ package com.ala24.bookstore.web.memberdto;
 
 import com.ala24.bookstore.domain.Address;
 import com.ala24.bookstore.domain.Member;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class MemberFormDto {
 
 	@NotEmpty(message = "회원 이름은 필수 입니다.")
 	private String loginId;
 
 	@NotEmpty(message = "비밀번호는 4 ~ 16 자리여야 합니다.")
-	@Min(4)
-	@Max(16)
+	@Size(min = 4, max = 16)
 	private String password;
 
 	@NotEmpty(message = "이름은 필수 입니다.")
@@ -27,6 +27,10 @@ public class MemberFormDto {
 	private String city;
 	private String address;
 	private Integer zipcode;
+
+	public MemberFormDto() {
+
+	}
 
 	public Member toEntity() {
 		return Member.builder()
