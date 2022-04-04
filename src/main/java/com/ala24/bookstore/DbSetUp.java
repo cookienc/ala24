ee8fbfafa70a808b7210a7da0ed36acc7800452a
kookiedncream <kim76744@naver.com>
2022-04-04 19:21:08 +0900

    feat: Sample Item Data 입력

--- src/main/java/com/ala24/bookstore/DbSetUp.java
+++ src/main/java/com/ala24/bookstore/DbSetUp.java
@@ -3,7 +3,11 @@ package com.ala24.bookstore;
 import com.ala24.bookstore.domain.Address;
 import com.ala24.bookstore.domain.Cash;
 import com.ala24.bookstore.domain.Member;
+import com.ala24.bookstore.domain.item.Novel;
+import com.ala24.bookstore.domain.item.Poem;
+import com.ala24.bookstore.domain.item.SelfDevelopment;
 import com.ala24.bookstore.domain.type.MemberStatus;
+import com.ala24.bookstore.service.ItemService;
 import com.ala24.bookstore.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
@@ -18,11 +22,13 @@ import javax.annotation.PreDestroy;
 public class DbSetUp {
 
 	private final MemberService memberService;
+	private final ItemService itemService;
 	private final DataBaseCleanup dataBaseCleanup;
 
 	@PostConstruct
 	public void setUp() {
 		memberSetUp();
+		itemSetUp();
 	}
 
 	@PreDestroy
@@ -54,5 +60,68 @@ public class DbSetUp {
 		memberService.join(admin);
 	}
 
+	private void itemSetUp() {
+		Novel novel1 = Novel.builder()
+				.name("기사단장죽이기1")
+				.author("무라카미 하루키")
+				.publisher("문학동네")
+				.price(14670)
+				.stockQuantity(800)
+				.build();
+		Novel novel2 = Novel.builder()
+				.name("기사단장죽이기2")
+				.author("무라카미 하루키")
+				.publisher("문학동네")
+				.price(14670)
+				.stockQuantity(1000)
+				.build();
+
+		Poem poem1 = Poem.builder()
+				.name("꽃을 보듯 너를 본다")
+				.author("나태주")
+				.publisher("지혜")
+				.price(9000)
+				.stockQuantity(500)
+				.build();
+		Poem poem2 = Poem.builder()
+				.name("윤동주 전 시집 하늘과 바람과 별과 시")
+				.author("윤동주")
+				.publisher("스타북스")
+				.price(117000)
+				.stockQuantity(1500)
+				.build();
+
+		SelfDevelopment sd1 = SelfDevelopment.builder()
+				.name("자바 ORM 표준 JPA 프로그래밍")
+				.author("김영한")
+				.publisher("에이콘출판사")
+				.price(38700)
+				.stockQuantity(5000)
+				.build();
+
+		SelfDevelopment sd2 = SelfDevelopment.builder()
+				.name("토비의 스프링 3.1 Vol. 1 스프링의 이해와 원리")
+				.author("이일민")
+				.publisher("에이콘출판사")
+				.price(3600)
+				.stockQuantity(500)
+				.build();
+
+		SelfDevelopment sd3 = SelfDevelopment.builder()
+				.name("토비의 스프링 3.1 Vol. 2 스프링의 기술과 선택")
+				.author("이일민")
+				.publisher("에이콘출판사")
+				.price(3600)
+				.stockQuantity(500)
+				.build();
+
+		itemService.saveItem(novel1);
+		itemService.saveItem(novel2);
+		itemService.saveItem(poem1);
+		itemService.saveItem(poem2);
+		itemService.saveItem(sd1);
+		itemService.saveItem(sd2);
+		itemService.saveItem(sd3);
+	}
 
 }
