478925a05f8e3508957d2893e8a0e25da2bb1f27
kookiedncream <kim76744@naver.com>
2022-04-05 21:46:17 +0900

    test: 주문 폼 테스트 추가

---
+++ src/test/java/com/ala24/bookstore/web/controller/order/OrderControllerTest.java
@@ -0,0 +1,73 @@
+package com.ala24.bookstore.web.controller.order;
+
+import com.ala24.bookstore.DataBaseCleanup;
+import com.ala24.bookstore.domain.Address;
+import com.ala24.bookstore.domain.Cash;
+import com.ala24.bookstore.domain.Member;
+import com.ala24.bookstore.domain.item.Novel;
+import com.ala24.bookstore.service.ItemService;
+import com.ala24.bookstore.service.MemberService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderControllerTest {
+
+	@Autowired
+	MockMvc mvc;
+
+	@Autowired
+	MemberService memberService;
+
+	@Autowired
+	ItemService itemService;
+
+	@Autowired
+	DataBaseCleanup dataBaseCleanup;
+
+	@AfterEach
+	void tearDown() {
+		dataBaseCleanup.execute();
+	}
+
+	@Test
+	void 주문_폼으로_이동() throws Exception {
+	    //given
+		Member test = Member.builder()
+				.name("test")
+				.loginId("test")
+				.password("test")
+				.cash(Cash.charge(0L))
+				.address(Address.builder()
+						.zipCode(12345)
+						.city("Seoul")
+						.specificAddress("Apartment")
+						.build())
+				.build();
+		Novel novel = Novel.builder()
+				.name("기사단장죽이기1")
+				.author("무라카미 하루키")
+				.publisher("문학동네")
+				.price(14670)
+				.stockQuantity(800)
+				.build();
+
+		memberService.join(test);
+		itemService.saveItem(novel);
+	    //when
+		mvc.perform(get("/order/1")
+						.sessionAttr("loginMember", test))
+				//then
+				.andExpect(status().isOk())
+				.andExpect(view().name("order/orderForm"));
+	}
+}
